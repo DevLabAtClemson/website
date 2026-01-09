8f01cba318085eca97323d3eccad53c16e0199c3
Paige Rodeghero <1682644+paigerodeghero@users.noreply.github.com>
2026-01-09 12:15:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: "/",   // root of the domain
-});
+  plugins: [react()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+})
