3ea6332ccb5ecfcef9e305c391b141afd26607ed
Paige Rodeghero <1682644+paigerodeghero@users.noreply.github.com>
2026-02-07 16:38:36 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <img
               src="/devlab-header.png"
               alt="DevLab at Clemson University"
-              className="h-10 w-auto"
+              className="h-12 w-auto"
             />
           </a>
 
